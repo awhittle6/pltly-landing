fa3e0559a879e5a0352c4f3eaf00e066638f5f69
awhittle6 <awhittlex2@gmail.com>
2025-02-10 11:09:08 -0800

    favicon and metadata

---
+++ components/common/Icon.tsx
@@ -0,0 +1,10 @@
+import { Utensils } from 'lucide-react'
+import React from 'react'
+
+const Icon = () => {
+  return (
+    <Utensils className="h-8 w-8 text-primary" />
+  )
+}
+
+export default Icon
